33caed2329f1562e3d5f70fa199f41612ecdfd86
Stepan Chekan <darilldev@gmail.com>
2026-06-08 02:19:16 +0800

    feat(internal-layout): implement sidebar + content layout

--- src/layouts/internal/internal-layout.component.spec.ts
+++ src/layouts/internal/internal-layout.component.spec.ts
@@ -1,5 +1,6 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import type { ComponentFixture } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { InternalLayoutComponent } from './internal-layout.component';
 
 describe('InternalLayoutComponent', () => {
@@ -9,6 +10,7 @@ describe('InternalLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [InternalLayoutComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(InternalLayoutComponent);
--- src/layouts/internal/internal-layout.component.ts
+++ src/layouts/internal/internal-layout.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-internal-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './internal-layout.component.html',
   styleUrl: './internal-layout.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class InternalLayoutComponent {}
