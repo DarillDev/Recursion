55cdf89d00bde24e290db33029efda1a03f76333
Stepan Chekan <darilldev@gmail.com>
2026-06-08 02:17:14 +0800

    feat(ds): add sidebar icons to SVG sprite and EIconName

---
+++ src/shared/ui-kit/icon/enums/icon-name.enum.ts
@@ -0,0 +1,14 @@
+export enum EIconName {
+  Edit = 'edit',
+  Mail = 'mail',
+  Location = 'location',
+  Calendar = 'calendar',
+  ChevronDown = 'chevron-down',
+  Close = 'close',
+  Search = 'search',
+  MenuOpen = 'menu-open',
+  Schema = 'schema',
+  Settings = 'settings',
+  AccountCircle = 'account-circle',
+  Logout = 'logout',
+}
